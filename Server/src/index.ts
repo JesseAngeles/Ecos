41e2de4b3128f39247aed2f5820fa361445d7351
Jesse <73973352+JesseAngeles@users.noreply.github.com>
2024-06-28 21:50:01 -0600

    Se termina el cliente web para consultar npcs

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,14 +1,19 @@
-import express, { Router } from "express";
-import connectDB from "./database";
-import routerNpc from "./Routes/npc";
+import express from 'express';
+import cors from 'cors'; // Importar el middleware CORS
+import connectDB from './database';
+import routerNpc from './Routes/npc';
 
-// Conexión con la base de datos
-connectDB();
+connectDB(); // Conexión con la base de datos
 
 const app = express();
+const port = 8085;
+
 app.use(express.json());
+app.use(cors()); // Usar CORS como middleware global
 
 // Definición de rutas
-app.use('/npc/', routerNpc);
+app.use('/npc', routerNpc);
 
-app.listen(8085, () => console.log("active server"));
+app.listen(port, () => {
+  console.log(`Servidor activo en el puerto ${port}`);
+});
